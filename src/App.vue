0c0ee5d3bf47bf8a916187121101dea31401a657
Rick Maya <rickmaya@gmail.com>
2021-04-22 19:43:27 -0500

    add warm and cold bg images.  style application background with linear-gradient

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <main>
-    <div class="search-box">
-      <input type="text" class="search-bar" placeholder="Search..." />
-    </div>
-  </main>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -19,5 +21,27 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/warm-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
 </style>
